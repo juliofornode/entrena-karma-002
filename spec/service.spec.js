fcd8a0f42c7550adadfc7a258454dfb478883f17
Julio Colomer <juliocolomer@unknown48d705c73613.att.net>
2016-03-07 13:03:24 -0800

    section completed

--- spec/service.spec.js
+++ spec/service.spec.js
@@ -1,20 +1,22 @@
 describe('omdb service', function () {
 
-    var movieData = '{"Title":"Star Wars","Year":"1983","Rated":"N/A","Released":"01 May 1983","Runtime":"N/A","Genre":"Action, Adventure, Sci-Fi","Director":"N/A","Writer":"N/A","Actors":"Harrison Ford, Alec Guinness, Mark Hamill, James Earl Jones","Plot":"N/A","Language":"English","Country":"USA","Awards":"N/A","Poster":"N/A","Metascore":"N/A","imdbRating":"7.8","imdbVotes":"349","imdbID":"tt0251413","Type":"game","Response":"True"}';
+    var omdbService = {};
+    var movieData = {"Title":"Star Wars","Year":"1983","Rated":"N/A","Released":"01 May 1983","Runtime":"N/A","Genre":"Action, Adventure, Sci-Fi","Director":"N/A","Writer":"N/A","Actors":"Harrison Ford, Alec Guinness, Mark Hamill, James Earl Jones","Plot":"N/A","Language":"English","Country":"USA","Awards":"N/A","Poster":"N/A","Metascore":"N/A","imdbRating":"7.8","imdbVotes":"349","imdbID":"tt0251413","Type":"game","Response":"True"};
+    var movieDataById = {"Title":"Star Wars","Year":"1983","Rated":"N/A","Released":"01 May 1983","Runtime":"N/A","Genre":"Action, Adventure, Sci-Fi","Director":"N/A","Writer":"N/A","Actors":"Harrison Ford, Alec Guinness, Mark Hamill, James Earl Jones","Plot":"N/A","Language":"English","Country":"USA","Awards":"N/A","Poster":"N/A","Metascore":"N/A","imdbRating":"7.8","imdbVotes":"349","imdbID":"tt0251413","Type":"game","Response":"True"};
+
+    beforeEach(module('omdb'));
+
+    beforeEach(inject(function(_omdbService_) {
+        omdbService = _omdbService_;
+    }));
 
     it('should return the search result', function () {
-
-        var omdbService = {};
-
-        angular.mock.module('omdb');
-
-        angular.mock.inject(function(_omdbService_) {
-            omdbService = _omdbService_;
-        });
-
+        //console.log(angular.mock.dump(movieData));
         expect(omdbService.search('Star Wars')).toEqual(movieData);
+    });
 
-    })
-
+    it('should return the searchById result', function () {
+        expect(omdbService.searchById('Star Wars')).toEqual(movieData);
+    });
 
 });
