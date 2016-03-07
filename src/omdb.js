1418fbab2a87df8146535926eb19d03304e1a467
Julio <juliofornode@gmail.com>
2016-03-07 13:26:22 -0800

    initial  testing exercise}

--- src/omdb.js
+++ src/omdb.js
@@ -3,14 +3,20 @@
     "use strict";
 
     angular.module('omdb', [])
-        .factory('omdbService', function () {
+        .factory('omdbService', function ($http, $q) {
 
             var movieData = {"Title":"Star Wars","Year":"1983","Rated":"N/A","Released":"01 May 1983","Runtime":"N/A","Genre":"Action, Adventure, Sci-Fi","Director":"N/A","Writer":"N/A","Actors":"Harrison Ford, Alec Guinness, Mark Hamill, James Earl Jones","Plot":"N/A","Language":"English","Country":"USA","Awards":"N/A","Poster":"N/A","Metascore":"N/A","imdbRating":"7.8","imdbVotes":"349","imdbID":"tt0251413","Type":"game","Response":"True"};
             var movieDataById = {"Title":"Star Wars","Year":"1983","Rated":"N/A","Released":"01 May 1983","Runtime":"N/A","Genre":"Action, Adventure, Sci-Fi","Director":"N/A","Writer":"N/A","Actors":"Harrison Ford, Alec Guinness, Mark Hamill, James Earl Jones","Plot":"N/A","Language":"English","Country":"USA","Awards":"N/A","Poster":"N/A","Metascore":"N/A","imdbRating":"7.8","imdbVotes":"349","imdbID":"tt0251413","Type":"game","Response":"True"};
+            var baseUrl = 'http://www.omdbapi.com/?v=1&';
 
             return {
                 search: function (query) {
-                    return movieData;
+                    var myPromise = $q.defer();
+                    $http.get(baseUrl + 's=' + encodeURIComponent(query))
+                        .success(function (data) {
+                            myPromise.resolve(data);
+                        });
+                    return myPromise.promise;
                 },
 
                 searchById: function (query) {
