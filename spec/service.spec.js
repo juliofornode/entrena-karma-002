a45427556459dec75b878f06380c8002a7efb736
Julio Colomer <juliocolomer@unknown48d705c73613.att.net>
2016-03-07 14:33:55 -0800

    working on  testing

--- spec/service.spec.js
+++ spec/service.spec.js
@@ -3,16 +3,30 @@ describe('omdb service', function () {
     var omdbService = {};
     var movieData = {"Title":"Star Wars","Year":"1983","Rated":"N/A","Released":"01 May 1983","Runtime":"N/A","Genre":"Action, Adventure, Sci-Fi","Director":"N/A","Writer":"N/A","Actors":"Harrison Ford, Alec Guinness, Mark Hamill, James Earl Jones","Plot":"N/A","Language":"English","Country":"USA","Awards":"N/A","Poster":"N/A","Metascore":"N/A","imdbRating":"7.8","imdbVotes":"349","imdbID":"tt0251413","Type":"game","Response":"True"};
     var movieDataById = {"Title":"Star Wars","Year":"1983","Rated":"N/A","Released":"01 May 1983","Runtime":"N/A","Genre":"Action, Adventure, Sci-Fi","Director":"N/A","Writer":"N/A","Actors":"Harrison Ford, Alec Guinness, Mark Hamill, James Earl Jones","Plot":"N/A","Language":"English","Country":"USA","Awards":"N/A","Poster":"N/A","Metascore":"N/A","imdbRating":"7.8","imdbVotes":"349","imdbID":"tt0251413","Type":"game","Response":"True"};
+    var $httpBackend;
 
     beforeEach(module('omdb'));
 
-    beforeEach(inject(function(_omdbService_) {
+    beforeEach(inject(function(_omdbService_, _$httpBackend_) {
         omdbService = _omdbService_;
+        $httpBackend = _$httpBackend_;
     }));
 
     it('should return the search result', function () {
         //console.log(angular.mock.dump(movieData));
-        expect(omdbService.search('Star Wars')).toEqual(movieData);
+        var response;
+
+        $httpBackend.when('GET', 'http://www.omdbapi.com/?v=1&s=Star%20Wars')
+            .respond(200, movieData);
+
+        omdbService.search('Star Wars')
+            .then(function (data) {
+                response = data;
+            });
+
+        $httpBackend.flush();
+
+        expect(response).toEqual(movieData);
     });
 
     it('should return the searchById result', function () {
